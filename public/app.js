042346828618d2cb56be0d0b6d8ba9cde09f7d1c
tom-gora <goratomasz@outlook.com>
2023-08-01 00:29:49 +0100

    saving progress on horizontal ui

--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
 window.onload = () => {
+  const navigation = document.querySelector(".links-navbar");
+  const navigationToggle = navigation.querySelector(".toggle-navbar");
+  const navigationIcon = navigationToggle.querySelector("ion-icon");
+
+  const typewriter = document.querySelector(".typewriter h3");
+
   const toggles = document.querySelectorAll(".chevron-hint");
   const cards = document.querySelectorAll(".card");
 
@@ -25,23 +31,27 @@ window.onload = () => {
     const cardThumbnail = parentCard.querySelector(".thumb");
 
     let cardSize = parentCard.getAttribute("data-size");
-    let cardState = parentCard.getAttribute("aria-expanded");
+    let cardState = parentCard.getAttribute("data-expanded");
 
     if (cardSize === "small" && cardState === "false") {
+      parentCard.setAttribute("data-expanded", "true");
       parentCard.setAttribute("aria-expanded", "true");
       parentFooter.style.bottom = 0;
       element.style.transform = "rotate(180deg)";
     } else if (cardSize === "small" && cardState === "true") {
+      parentCard.setAttribute("data-expanded", "false");
       parentCard.setAttribute("aria-expanded", "false");
-      parentFooter.style.bottom = "-9rem";
+      parentFooter.style.bottom = "-8rem";
       element.style.transform = "rotate(0deg)";
     } else if (cardSize === "large" && cardState === "false") {
       parentFooter.style.bottom = 0;
+      parentCard.setAttribute("data-expanded", "true");
       parentCard.setAttribute("aria-expanded", "true");
       cardThumbnail.style.height = "25rem";
       element.style.transform = "rotate(0deg)";
     } else if (cardSize === "large" && cardState === "true") {
       parentFooter.style.bottom = 0;
+      parentCard.setAttribute("data-expanded", "false");
       parentCard.setAttribute("aria-expanded", "false");
       cardThumbnail.style.height = 0;
       element.style.transform = "rotate(180deg)";
@@ -56,7 +66,6 @@ window.onload = () => {
   });
 
   const resetCards = () => {
-    console.log("data-size attribute has changed!");
     location.reload();
   };
   const observer = new MutationObserver((mutationsList) => {
@@ -77,4 +86,29 @@ window.onload = () => {
     attributeFilter: ["data-size"],
     attributeOldValue: true, // Include the old value in the mutation object
   });
+
+  const handleMobileNav = () => {
+    const menuState = navigation.getAttribute("data-expanded");
+    if (menuState === "false") {
+      navigation.setAttribute("data-expanded", "true");
+      navigationIcon.setAttribute("name", "close-circle");
+    } else {
+      navigation.setAttribute("data-expanded", "false");
+      navigationIcon.setAttribute("name", "chevron-back-circle");
+    }
+  };
+
+  navigationToggle.addEventListener("click", handleMobileNav);
+  const animateTypewriter = () => {
+    typewriter.setAttribute("data-animate", "true");
+  };
+
+  var typewriterObserver = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting === true) animateTypewriter();
+    },
+    { threshold: [0.5] }
+  );
+
+  typewriterObserver.observe(typewriter);
 };
