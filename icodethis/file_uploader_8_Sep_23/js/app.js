001623295e2bea126f9c0e26c8940c8964b04762
tom-gora <goratomasz@outlook.com>
2023-09-18 09:46:35 +0100

    finished product

--- icodethis/file_uploader_8_Sep_23/js/app.js
+++ icodethis/file_uploader_8_Sep_23/js/app.js
@@ -16,16 +16,17 @@ const getUploadItemHTML = (fileExtension, fileName, time) => {
                 <button>View File</button>
               </div>
             </div>
-            <div class="stop-download">
+            <div class="stop-delete">
               <ion-icon name="close-outline"></ion-icon>
             </div>
           </li>`;
 };
 
 const fileInput = document.querySelector("input[type='file']");
-const dragAndDropArea = document.querySelector(".uploader");
+const dragAndDropArea = document.querySelector(".drag-and-drop-area");
+const dragAndDropUploader = document.querySelector(".uploader");
 const uploadButton = document.querySelector(".select-files");
-const cloudIndicator = document.querySelector(".uploader svg");
+const cloudIndicator = document.querySelector(".uploader svg path");
 const errorMessage = document.querySelector("#error-message");
 
 const truncateFileName = (string) => {
@@ -33,18 +34,15 @@ const truncateFileName = (string) => {
   else return string;
 };
 
-/* TODO: fileHandler must calc time from filesize then store in "innerHTML" as data attr
-so it can be read in mutation observer and animate accordingly with CSS
-*/
 const fileHandler = (file) => {
   if (file.size > 52428800) {
     errorMessage.innerText = "You can upload files up to 50MB in size";
     return;
   }
 
-  // calculate artitrary time as 10s per mb of file read
+  // calculate arbitrary time as 20s per mb of file read
   const megaBytes = file.size / 1048576;
-  const timeInSeconds = megaBytes * 10;
+  const timeInSeconds = Math.floor(megaBytes * 20000);
 
   errorMessage.innerText = "";
   const reader = new FileReader();
@@ -72,6 +70,8 @@ uploadButton.addEventListener("click", () => {
   fileInput.click();
 });
 
+const observerConfig = { childList: true, subtree: false };
+
 const observer = new MutationObserver((mutationList, observer) => {
   mutationList.forEach((mutation) => {
     if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
@@ -80,12 +80,72 @@ const observer = new MutationObserver((mutationList, observer) => {
         const bar = addedNode.querySelector(".bar");
         const counter = addedNode.querySelector(".progress-counter");
         bar.classList.add("bar-moving");
-        const time = addedNode.getAttribute("data-time");
-        console.log(addedNode);
-        console.log(time);
+        const timeToAnimate = addedNode.getAttribute("data-time");
+
+        const viewButton = addedNode.querySelector(".controls :nth-child(3)");
+        const stopDeleteButton = addedNode.querySelector(".stop-delete");
+        const stopDeleteIcon = addedNode.querySelector(".stop-delete ion-icon");
+
+        uploadListContainer.scrollIntoView(false);
+
+        stopDeleteButton.addEventListener("click", () => {
+          uploadListContainer.removeChild(addedNode);
+        });
+
+        bar.style.setProperty("--animation-time", timeToAnimate + "ms");
+        counter.style.setProperty("--animation-time", timeToAnimate + "ms");
+
+        const countTo100 = (node, time) => {
+          const interval = time / 100;
+          let currentStep = 0;
+
+          const intervalId = setInterval(() => {
+            if (currentStep <= 100) {
+              node.innerText = `${currentStep}%`;
+              currentStep++;
+            } else {
+              clearInterval(intervalId);
+              node.style.left = "50%";
+              node.style.color = "var(--card-bg)";
+              node.style.fontWeight = "900";
+              viewButton.style.display = "block";
+              stopDeleteIcon.setAttribute("name", "trash-outline");
+            }
+          }, interval);
+        };
+
+        countTo100(counter, timeToAnimate);
       });
-    }
+    } else return;
   });
 });
 
-observer.observe(uploadListContainer, { childList: true, subtree: true });
+observer.observe(uploadListContainer, observerConfig);
+
+dragAndDropArea.addEventListener("dragenter", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  dragAndDropUploader.classList.add("active");
+});
+
+dragAndDropArea.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  dragAndDropUploader.classList.remove("active");
+});
+
+dragAndDropArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  dragAndDropUploader.classList.add("active");
+});
+
+dragAndDropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  dragAndDropUploader.classList.remove("active");
+  let files = e.dataTransfer.files;
+  Array.from(files).forEach((file) => {
+    fileHandler(file);
+  });
+});
